5d81c89a26a050a223b5f55520e052e477f3ab76
muriloartbc-art <murilo.art.bc@gmail.com>
2026-05-20 03:25:02 -0300

    Remove hardcoded env values from repo

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,22 +1,6 @@
 import serverless from "serverless-http";
-import express from "express";
-import { configureApp } from "../../server";
+import { createApiApp } from "../../api";
 
-const lambdaApp = express();
-
-let configuredAppPromise: Promise<express.Express> | null = null;
-
-lambdaApp.use(async (req, res, next) => {
-  try {
-    if (!configuredAppPromise) {
-      configuredAppPromise = configureApp();
-    }
-
-    const configuredApp = await configuredAppPromise;
-    return configuredApp(req, res, next);
-  } catch (error) {
-    next(error);
-  }
-});
+const lambdaApp = createApiApp();
 
 export const handler = serverless(lambdaApp);
